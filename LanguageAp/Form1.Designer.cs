bea57ca67c52803e0866f2015353249577c61d73
Arun Geo Koshy <Arungeokoshy@arstraffic.com>
2022-05-13 10:34:17 +0530

    Add project files.

---
+++ LanguageAp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LanguageAp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Language = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(388, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(388, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(190, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(190, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ADDRESS";
+            // 
+            // Language
+            // 
+            this.Language.FormattingEnabled = true;
+            this.Language.Items.AddRange(new object[] {
+            "MALAYALAM",
+            "ENGLISH",
+            "TAMIL"});
+            this.Language.Location = new System.Drawing.Point(652, 23);
+            this.Language.Name = "Language";
+            this.Language.Size = new System.Drawing.Size(121, 21);
+            this.Language.TabIndex = 4;
+            this.Language.Text = "MALAYALAM";
+            this.Language.SelectedIndexChanged += new System.EventHandler(this.Language_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(538, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LANGUAGE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(253, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 37);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "WELCOME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Language);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox Language;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LanguageAp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LanguageAp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+
+        }
+
+        private void Language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Language.Text == "MALAYALAM")
+            {
+                ResourceManager rm = new ResourceManager("LanguageAp.resmal", Assembly.GetExecutingAssembly());
+                label4.Text = rm.GetString("Welcome");
+                label2.Text = rm.GetString("Address");
+                label1.Text = rm.GetString("Name");
+                label3.Text = rm.GetString("Language");
+              
+            }
+            else if (Language.Text == "TAMIL")
+            {
+                ResourceManager rm = new ResourceManager("LanguageAp.restam", Assembly.GetExecutingAssembly());
+                label4.Text = rm.GetString("Welcome");
+                label2.Text = rm.GetString("Address");
+                label1.Text = rm.GetString("Name");
+                label3.Text = rm.GetString("Language");
+               
+            }
+
+            else if (Language.Text == "ENGLISH")
+            {
+                label1.Text = "NAME";
+                label2.Text = "ADDRESS";
+                label4.Text = "WELCOME";
+                label3.Text = "LANGUAGE";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResourceManager rm = new ResourceManager("LanguageAp.resmal", Assembly.GetExecutingAssembly());
+            label4.Text = rm.GetString("Welcome");
+            label2.Text = rm.GetString("Address");
+            label1.Text = rm.GetString("Name");
+            label3.Text = rm.GetString("Language");
+        }
+    }
+}
